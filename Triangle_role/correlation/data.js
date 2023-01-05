7d3aa63b4ef243b18d70bf97b6e3c28d98b708f3
Will Enny <enny.williamrobert@gmail.com>
2023-01-04 21:23:35 -0500

    organization and beginning correlation bar chart.

---
+++ Triangle_role/correlation/data.js
@@ -0,0 +1,149 @@
+var featureCorrelations = [
+  {
+    "Code": "cases",
+    "Feature": "COVID-19 Cases",
+    "Correlation": 0.1988,
+    "Increase_from_2016": "39208",
+    "population": "1511946"
+  },
+  {
+    "Code": "fips",
+    "Feature": "Federal Information Processing System Code",
+    "Correlation": -0.01,
+    "Increase_from_2016": "34036",
+    "population": "1626078"
+  },
+  {
+    "Code": "pcincome",
+    "Feature": "",
+    "Correlation": 0.72585,
+    "Increase_from_2016": "23935",
+    "population": "1341075"
+  },
+  {
+    "Code": "popestimate",
+    "Feature": "Fort Worth",
+    "Correlation": 0.3449,
+    "Increase_from_2016": "20664",
+    "population": "874168"
+  },
+  {
+    "Code": "mfratio",
+    "Feature": "Los Angeles",
+    "Correlation": -0.0463,
+    "Increase_from_2016": "18643",
+    "population": "3999759"
+  },
+  {
+    "Code": "popest_male",
+    "Feature": "Seattle",
+    "Correlation": 0.3461,
+    "Increase_from_2016": "17490",
+    "population": "724745"
+  },
+  {
+    "Code": "popest_fem",
+    "Feature": "Charlotte",
+    "Correlation": 0.3437,
+    "Increase_from_2016": "15551",
+    "population": "859035"
+  },
+  {
+    "Code": "median_age_tot",
+    "Feature": "Columbus ",
+    "Correlation": -0.01746,
+    "Increase_from_2016": "15429",
+    "population": "879170"
+  },
+  {
+    "Code": "under5_tot",
+    "Feature": "Frisco",
+    "Correlation": 0.3236,
+    "Increase_from_2016": "13470",
+    "population": "177286"
+  },
+  {
+    "Code": "age513_tot",
+    "Feature": "Atlanta",
+    "Correlation": 0.3247,
+    "Increase_from_2016": "13323",
+    "population": "486290"
+  },
+  {
+    "Code": "age1544_tot",
+    "Feature": "San Diego",
+    "Correlation": 0.3411,
+    "Increase_from_2016": "12834",
+    "population": "1419516"
+  },
+  {
+    "Code": "age4564_tot",
+    "Feature": "Austin ",
+    "Correlation": 0.3501,
+    "Increase_from_2016": "12515",
+    "population": "950715"
+  },
+  {
+    "Code": "age65plus_tot",
+    "Feature": "Jacksonville",
+    "Correlation": 0.3609,
+    "Increase_from_2016": "11169",
+    "population": "892062"
+  },
+  {
+    "Code": "rucc_2013",
+    "Feature": "Irvine",
+    "Correlation": -0.3193,
+    "Increase_from_2016": "11068",
+    "population": "277453"
+  },
+  {
+    "Code": "wa",
+    "Feature": "Henderson",
+    "Correlation": 0.3376,
+    "Increase_from_2016": "10534",
+    "population": "302539"
+  },
+  {
+    "Code": "ba",
+    "Feature": "Henderson",
+    "Correlation": 0.2006,
+    "Increase_from_2016": "10534",
+    "population": "302539"
+  },
+  {
+    "Code": "ia",
+    "Feature": "Henderson",
+    "Correlation": 0.2456,
+    "Increase_from_2016": "10534",
+    "population": "302539"
+  },
+  {
+    "Code": "aa",
+    "Feature": "Henderson",
+    "Correlation": 0.3848,
+    "Increase_from_2016": "10534",
+    "population": "302539"
+  },
+  {
+    "Code": "na",
+    "Feature": "Henderson",
+    "Correlation": 0.2746,
+    "Increase_from_2016": "10534",
+    "population": "302539"
+  },
+  {
+    "Code": "tom",
+    "Feature": "Henderson",
+    "Correlation": 0.3875,
+    "Increase_from_2016": "10534",
+    "population": "302539"
+  },
+  {
+    "Code": "h",
+    "Feature": "Henderson",
+    "Correlation": 0.2403,
+    "Increase_from_2016": "10534",
+    "population": "302539"
+  }
+ ]
